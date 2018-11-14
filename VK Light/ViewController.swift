5457f150b453f5f7b26e3c0342eca2d47a3b1a68
Иван Маслюк <ivanmasluk@ivans-imac.local>
2018-11-14 20:01:13 +0300

    Initial Commit

---
+++ VK Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Light
+//
+//  Created by Иван Маслюк on 14/11/2018.
+//  Copyright © 2018 Иван Маслюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
